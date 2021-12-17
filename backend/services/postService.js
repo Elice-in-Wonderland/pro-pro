682c8a7d55e2b78a5aa0ace068a72964b7c2e6dd
Danji-ya <erangerang15@gmail.com>
2021-12-17 15:30:51 +0900

    [feat]: 게시글 목록 및 페이지네이션 구현 1. 게시글 목록에 맞는 필요한 내용 반환 2. 게시글 페이지네이션

--- backend/services/postService.js
+++ backend/services/postService.js
@@ -1,5 +1,33 @@
 const postModel = require('../models/post');
 
+// 게시글 목록
+exports.getPost = async (category, skipSize, perPage) => {
+  let posts = await postModel
+    .find({ category })
+    .sort({ createdAt: -1 })
+    .skip(skipSize)
+    .limit(perPage);
+
+  posts = posts.map(post => {
+    const restructuredPost = {
+      _id: post._id,
+      category: post.category,
+      title: post.title,
+      recruitmentStatus: post.recruitmentStatus,
+      stacks: post.stacks,
+      sido: post.sido || '온라인',
+      capacity: post.capacity,
+      marks: post.marks,
+      views: post.views,
+      createdAt: post.createdAt,
+      registerDeadline: post.registerDeadline,
+    };
+    return restructuredPost;
+  });
+
+  return posts;
+};
+
 // 게시글 생성
 exports.createPost = async data => {
   const post = await postModel.create(data);
