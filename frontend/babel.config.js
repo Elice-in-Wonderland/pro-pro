5330675124075a2091ef43f6a5751f20a1c00ecc
Danji-ya <erangerang15@gmail.com>
2021-12-18 19:27:55 +0900

    [chore]: FE_webpack 설정

---
+++ frontend/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage", // 폴리필 사용 방식 지정
+        corejs: {
+          // 폴리필 버전 지정
+          version: 3,
+        },
+      },
+    ],
+  ],
+};
