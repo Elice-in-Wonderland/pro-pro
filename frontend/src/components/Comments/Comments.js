d6fa8aad3bc04c48457ac13a57e335ac7c75ff42
LEON <dlwotjr2@gmail.com>
2021-12-20 23:43:42 +0900

    [feat]: 댓글 컴포넌트 댓글, 대댓글 렌더링 1. 댓글 HTML 생성을 함수화 2. eslint, prettier 적용

--- frontend/src/components/Comments/Comments.js
+++ frontend/src/components/Comments/Comments.js
@@ -1,29 +1,42 @@
-import Component from "../component";
-import styles from "./comments.scss";
+import Component from '../component';
+import styles from './comments.scss';
 
 export default class stacks extends Component {
-    constructor(props){
-        super(props)
-        this.$dom = this.createDom('ul', {
-            className: "comments",
-        })
-        this._state = {
-            commentList : props.commentList,
-        };
-        this.render();
-    }
+  constructor(props) {
+    super(props);
+    this.$dom = this.createDom('div', {
+      className: 'comments',
+    });
+    this.state = {
+      commentList: props.commentList,
+    };
+    this.render();
+  }
 
-    render = () => {
-        this.$dom.innerHTML = this._state.commentList.map((item) => {
-            return `
-            <div class="userWrapper">
-                <img src=${item.imageURL} width="30px" height="30px" />
-                <h4 class="userName">${item.author}</h4>
-            </div>
-            <li class="comment">${item.content}</li>
-            <hr>
-            `
-        })
-        .join('');
-    }
+  makeCommentHTML = (comment, parent) => {
+    return `<div class=${parent === 'post' ? 'comment' : 'nestedComment'}>
+      <div class="userWrapper">
+          <img src=${comment.imageURL} width="30px" height="30px" />
+          <h4 class="userName">${comment.author}</h4>
+          <h5 class="commentedTime" >${comment.updatedAt.slice(0, 10)}</h5>
+      </div>
+      <h6 class="commentContent">${comment.content}</h6>
+  </div>
+      <hr>`;
+  };
+
+  render = () => {
+    this.$dom.innerHTML = this.state.commentList
+      .map(comment => {
+        if (comment.nestedComments) {
+          let nestedComments = ``;
+          comment.nestedComments.forEach(nestedComment => {
+            nestedComments += this.makeCommentHTML(nestedComment, 'comment');
+          });
+          return this.makeCommentHTML(comment, 'post') + nestedComments;
+        }
+        return this.makeCommentHTML(comment, 'post');
+      })
+      .join('');
+  };
 }
