4014fb17e48f1bb37b81b3551627dc879909cf70
LEON <dlwotjr2@gmail.com>
2021-12-21 20:38:41 +0900

    [feat]: 게시글 배너 컴포넌트 생성 1. 상세 게시글에서 보이는 기술스택 배경, 로고 구현

---
+++ frontend/src/components/PostBanner/PostBanner.js
@@ -0,0 +1,72 @@
+import Component from '../component';
+import styles from './postBanner.scss';
+import { defaultStacks } from '../../library/Profile';
+
+import cpp from '../../assets/icons/cpp.svg';
+import django from '../../assets/icons/django-icon.svg';
+import flutter from '../../assets/icons/flutter.svg';
+import go from '../../assets/icons/go.svg';
+import java from '../../assets/icons/java.svg';
+import javascript from '../../assets/icons/javascript.svg';
+import kotlin from '../../assets/icons/kotlin.svg';
+import nodejs from '../../assets/icons/nodejs-icon.svg';
+import python from '../../assets/icons/python.svg';
+import react from '../../assets/icons/react.svg';
+import spring from '../../assets/icons/spring.svg';
+import swift from '../../assets/icons/swift.svg';
+import typescript from '../../assets/icons/typescript.svg';
+import vue from '../../assets/icons/vue.svg';
+
+const stackLogos = [
+  cpp,
+  django,
+  flutter,
+  go,
+  java,
+  javascript,
+  kotlin,
+  nodejs,
+  python,
+  react,
+  spring,
+  swift,
+  typescript,
+  vue,
+];
+
+export default class PostBanner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stackList: props.stackList,
+      firstStack: props.stackList[0],
+    };
+
+    this.$dom = this.createDom('div', {
+      className: `banner ${this.state.firstStack}`,
+    });
+
+    this.state.stackLogoURLs = [];
+
+    this.state.stackList.forEach(stack => {
+      for (let i = 0; i < defaultStacks.length; i++) {
+        if (defaultStacks[i] === stack) {
+          this.state.stackLogoURLs.push(stackLogos[i]);
+          break;
+        }
+      }
+    });
+
+    this.render();
+  }
+
+  render = () => {
+    this.$dom.innerHTML = this.state.stackLogoURLs
+      .map(stack => {
+        return `
+        <img class="bannerLogo" src='${stack}' alt="${stack}" />
+        `;
+      })
+      .join('');
+  };
+}
