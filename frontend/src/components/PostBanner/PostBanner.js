d21d915d8816786fb181f4f0c3951f66a613b666
Jin1won <wlsdnjs16521@gmail.com>
2021-12-23 10:21:45 +0900

    [feat]: 북마크 기능 구현 1. 북마크 기능 구현 2. 반응형 디자인 3. 포스트 클릭시 디테일 페이지로 이동 기능 구현

--- frontend/src/components/PostBanner/PostBanner.js
+++ frontend/src/components/PostBanner/PostBanner.js
@@ -7,14 +7,14 @@ import django from '../../assets/icons/django-icon.svg';
 import flutter from '../../assets/icons/flutter.svg';
 import go from '../../assets/icons/go.svg';
 import java from '../../assets/icons/java.svg';
-import javascript from '../../assets/icons/javascript.svg';
+import JavaScript from '../../assets/icons/javascript.svg';
 import kotlin from '../../assets/icons/kotlin.svg';
 import nodejs from '../../assets/icons/nodejs-icon.svg';
 import python from '../../assets/icons/python.svg';
 import react from '../../assets/icons/react.svg';
 import spring from '../../assets/icons/spring.svg';
 import swift from '../../assets/icons/swift.svg';
-import typescript from '../../assets/icons/typescript.svg';
+import TypeScript from '../../assets/icons/typescript.svg';
 import vue from '../../assets/icons/vue.svg';
 
 const stackLogos = [
@@ -23,23 +23,24 @@ const stackLogos = [
   flutter,
   go,
   java,
-  javascript,
+  JavaScript,
   kotlin,
   nodejs,
   python,
   react,
   spring,
   swift,
-  typescript,
+  TypeScript,
   vue,
 ];
 
 export default class PostBanner extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       stackList: props.stackList,
-      firstStack: props.stackList[0],
+      firstStack: props.stackList[0].toLowerCase(),
     };
 
     this.$dom = this.createDom('div', {
@@ -50,7 +51,7 @@ export default class PostBanner extends Component {
 
     this.state.stackList.forEach(stack => {
       for (let i = 0; i < defaultStacks.length; i++) {
-        if (defaultStacks[i] === stack) {
+        if (defaultStacks[i] === stack.toLowerCase()) {
           this.state.stackLogoURLs.push(stackLogos[i]);
           break;
         }
