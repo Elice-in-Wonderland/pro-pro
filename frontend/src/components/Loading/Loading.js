24474529b08ee39fef4f1de499306e1c68df3841
LEON <dlwotjr2@gmail.com>
2021-12-25 15:12:55 +0900

    [feat]: 로딩 컴포넌트 추가

---
+++ frontend/src/components/Loading/Loading.js
@@ -0,0 +1,8 @@
+import './Loading.scss';
+
+const Loading = `<div class="LoadingContainer">
+<div class="LoadingCircle"><div class="LoadingText">Loading...</div>
+</div>
+</div>`;
+
+export default Loading;
