1faa1dd03bf5ff26be76e34e2857011f2eff4fed
sjy <stseo012@gmail.com>
2022-04-21 23:31:11 +0900

    [refactor]: 메인 페이지 util 함수 분리

---
+++ frontend/src/utils/filter.js
@@ -0,0 +1,45 @@
+/* eslint-disable import/prefer-default-export */
+export const availFiltter = dataList => {
+  const today = new Date();
+  const statelist = dataList.filter(
+    post => post.registerDeadline >= today.toISOString(),
+  );
+  return statelist;
+};
+
+export const populateSort = dataList => {
+  const statelist = dataList.sort(
+    (view1, view2) => -(parseFloat(view1.views) - parseFloat(view2.views)),
+  );
+  return statelist;
+};
+
+export const recentSort = dataList => {
+  const statelist = dataList.sort((a, b) => {
+    if (a.createdAt < b.createdAt) return 1;
+    if (a.createdAt > b.createdAt) return -1;
+    return 0;
+  });
+  return statelist;
+};
+
+export const toggleButton = (activated, deactivated) => {
+  activated.classList.add('activated');
+  deactivated.classList.remove('activated');
+};
+
+export function debounce(eventHandlerFunc, leading = true) {
+  let inDebounce;
+  return (...args) => {
+    const context = this;
+    const nowCall = leading && !inDebounce;
+    const later = () => {
+      inDebounce = null;
+      if (!leading) eventHandlerFunc.apply(context, args);
+    };
+
+    clearTimeout(inDebounce);
+    inDebounce = setTimeout(later, 500);
+    if (nowCall) eventHandlerFunc.apply(context, args);
+  };
+}
