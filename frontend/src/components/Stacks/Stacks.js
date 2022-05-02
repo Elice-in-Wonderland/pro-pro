bed72fc992354f0ddc473145075ab62b8e1a1267
LEON <dlwotjr2@gmail.com>
2022-05-02 11:33:41 +0900

    [Refactor]: Stacks Core Component 적용

--- frontend/src/components/Stacks/Stacks.js
+++ frontend/src/components/Stacks/Stacks.js
@@ -1,20 +1,13 @@
-import Component from '../component';
+import CustomComponent from '../CustomComponent';
 import './stacks.scss';
 
-export default class Stacks extends Component {
-  constructor(props) {
-    super(props);
-    this.$dom = this.createDom('ul', {
-      className: 'stackList',
-    });
-    this.detailRender();
-  }
-
-  detailRender = () => {
-    this.$dom.innerHTML = this.props.stacks
+export default class Stacks extends CustomComponent {
+  markup() {
+    const { stacks } = this.props;
+    return stacks
       .map(item => {
         return `<li>${item}</li>`;
       })
       .join('');
-  };
+  }
 }
