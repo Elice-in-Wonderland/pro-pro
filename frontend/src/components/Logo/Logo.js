c8760b4bf5446319955eed0679f675f4db25f349
Danji-ya <erangerang15@gmail.com>
2022-05-05 09:47:47 +0900

    [refactor]: Logo 컴포넌트 JSX문법으로 수정

--- frontend/src/components/Logo/Logo.js
+++ frontend/src/components/Logo/Logo.js
@@ -3,6 +3,6 @@ import CustomComponent from '../CustomComponent';
 
 export default class Logo extends CustomComponent {
   markup() {
-    return `<img src=${logo} alt='logo' width='130px' hegiht='80px'>`;
+    return <img src={logo} alt="logo" width="130px" hegiht="80px" />;
   }
 }
