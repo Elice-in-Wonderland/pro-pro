355fe1059dd6c6c23ee8bd9d7691421d6112304c
LEON <dlwotjr2@gmail.com>
2022-05-06 11:17:14 +0900

    [Refactor]: CommentForm JSX 적용

--- frontend/src/components/CommentForm/CommentForm.js
+++ frontend/src/components/CommentForm/CommentForm.js
@@ -10,15 +10,26 @@ export default class CommentForm extends CustomComponent {
   }
 
   loggedUserForm() {
-    return `          
-        <textarea placeholder="댓글을 남겨주세요." class="writeComment" type="text" ></textarea>
+    return (
+      <fragment>
+        <textarea
+          placeholder="댓글을 남겨주세요."
+          class="writeComment"
+          type="text"
+        ></textarea>
         <input class="submitComment" type="submit" value="등록" />
-    `;
+      </fragment>
+    );
   }
 
   notLoggedUserForm() {
-    return `          
-        <textarea readonly placeholder="먼저 로그인 해주세요." class="writeComment" type="text" ></textarea>
-    `;
+    return (
+      <textarea
+        readonly
+        placeholder="먼저 로그인 해주세요."
+        class="writeComment"
+        type="text"
+      ></textarea>
+    );
   }
 }
